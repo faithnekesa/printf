ae669e295d5fbd81d10dbfd3f7140ac2b3ff00b0
faithnekesa <faynekesa@gmail.com>
2023-05-31 07:58:49 -0700

    updating main header file

--- main.h
+++ main.h
@@ -39,6 +39,31 @@ struct fmt
 
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
-
+int doPrint(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_char(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int getFlags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int handleWriteChar(char c, char buffer[],
+int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+int length, char padd, char extra_c);
+int write_unsgnd(int is_negative, int ind, char buffer[],
+int flags, int width, int precision, int size);
+int isDigit(char);
+int isPrintable(char);
 
 #endif
