08242b56ee811eac56a95bb6d5dc1249d3fdd7ab
faithnekesa <faynekesa@gmail.com>
2023-05-31 09:13:24 -0700

    Correcting printf functions file

--- printf_functions.c
+++ printf_functions.c
@@ -1,57 +1,185 @@
 #include "main.h"
-void printBuffer(char buffer[], int *buffer_index);
-/**
-* _printf - A function like printf function
-* @format:Formatted string to print arguments
-* Return:The number of chars printed
-*/
-int _printf(const char *format, ...)
-{
-int printedArgs = 0, counter = 0, charsPrinted = 0;
-int precision, flags, width, size, buffer_index = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return (-1);
-va_start(list, format);
-for (counter = 0; format && format[counter] != '\0'; counter++)
-{
-if (format[counter] != '%')
-{
-buffer[buffer_index++] = format[counter];
-if (buffer_index == BUFF_SIZE)
-printBuffer(buffer, &buffer_index);
 
-charsPrinted++;
-}
-else
+/**
+* print_char - Function that prints a character
+* @list: List a of arguments
+* @buffer: the buffer for array to handle printing
+* @flags: checks for active flags
+* @width: Width
+* @precision: Precision specification
+* @size: the size of the specifier
+* Return: the num of chars printed
+*/
+int print_char(va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-printBuffer(buffer, &buffer_index);
-flags = getFlags(format, &counter);
-precision = get_precision(format, &counter, list);
-width = get_width(format, &counter, list);
-size = get_size(format, &counter);
-++counter;
-printedArgs = doPrint(format, &counter, list, buffer,
-flags, width, precision, size);
-if (printedArgs == -1)
-return (-1);
-charsPrinted = charsPrinted +  printedArgs;
-}
-}
-printBuffer(buffer, &buffer_index);
-va_end(list);
-return (charsPrinted);
+	char c = va_arg(list, int);
+
+	return (handleWriteChar(c, buffer, flags, width, precision, size));
 }
 
 /**
-* printBuffer - Prints the contents of the buffer if it exist
-* @buffer: Array of chars
-* @buffer_index: Index for the next arg- the length
+* print_string -Function that prints a stringPtring
+* @list:argument list
+* @buffer: the buffer for array to handle printing
+* @flags: checks for active flags
+* @width: get width.
+* @precision: Precision specifier
+* @size: Size of the specifier
+* Return: The number of chars printed
 */
-void printBuffer(char buffer[], int *buffer_index)
+int print_string(va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-if (*buffer_index > 0)
-write(1, &buffer[0], *buffer_index);
-*buffer_index = 0;
+	int length = 0, counter;
+	char *stringPtr = va_arg(list, char *);
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (stringPtr == NULL)
+	{
+		stringPtr = "(null)";
+		if (precision >= 6)
+			stringPtr = "      ";
+	}
+
+	while (stringPtr[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & F_MINUS_)
+		{
+			write(1, &stringPtr[0], length);
+			for (counter = width - length; counter > 0; counter--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (counter = width - length; counter > 0; counter--)
+				write(1, " ", 1);
+			write(1, &stringPtr[0], length);
+			return (width);
+		}
+	}
+
+	return (write(1, stringPtr, length));
 }
+
+/**
+* print_percent -Function that prints %
+* @list:argument list
+* @buffer: the buffer for array to handle printing
+* @flags: checks for active flags
+* @width: get width.
+* @precision: Precision specifier
+* @size: Size of the specifier
+* Return: The number of chars printed
+*/
+int print_percent(va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(list);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+
+/**
+* print_int - Print int
+* @list:argument list
+* @buffer: the buffer for array to handle printing
+* @flags: checks for active flags
+* @width: get width.
+* @precision: Precision specifier
+* @size: Size of the specifier
+* Return: The number of chars printed
+*/
+int print_int(va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int counter = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int num1 = va_arg(list, long int);
+	unsigned long int num;
+
+	if (num1 == 0)
+		buffer[counter--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)num1;
+
+	if (num1 < 0)
+	{
+		num = (unsigned long int)((-1) * num1);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[counter--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	counter++;
+
+	return (write_number(is_negative, counter, buffer, flags,
+				width, precision, size));
+}
+
+/**
+* print_binary - Prints an unsigned number
+* @list:argument list
+* @buffer: the buffer for array to handle printing
+* @flags: checks for active flags
+* @width: get width.
+* @precision: Precision specifier
+* @size: Size of the specifier
+* Return: The number of chars printed
+*/
+int print_binary(va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int num1, num2, counter, sum;
+	unsigned int num3[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	num1 = va_arg(list, unsigned int);
+	num2 = 2147483648; /* (2 ^ 31) */
+	num3[0] = num1 / num2;
+	for (counter = 1; counter < 32; counter++)
+	{
+		num2 /= 2;
+		num3[counter] = (num1 / num2) % 2;
+	}
+	for (counter = 0, sum = 0, count = 0; counter < 32; counter++)
+	{
+		sum += num3[counter];
+		if (sum || counter == 31)
+		{
+			char newChar = '0' + num3[counter];
+
+			write(1, &newChar, 1);
+			count++;
+		}
+	}
+	return (count);
+}
+
